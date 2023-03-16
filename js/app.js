4e0e74ead0c45a675b250acc69d9e51ed752e95d
taimul <taimul.haque@gmail.com>
2023-03-16 16:46:30 +0600

    add an float animation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+window.addEventListener("scroll", function () {
+  var scrollText = document.getElementById("scroll-text");
+  var scrollPosition = window.scrollY + window.innerHeight;
+  var elementPosition = scrollText.offsetTop + scrollText.offsetHeight;
+
+  if (scrollPosition > elementPosition) {
+    scrollText.classList.remove("hidden");
+  }
+});
+
 var typed = new Typed(".auto-type", {
   strings: ["Team Members"],
   typeSpeed: 150,
